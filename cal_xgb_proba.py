21469fb1718519a85488586d6c69dc83ee70808a
bestfocus <51957682+bestfocus@users.noreply.github.com>
2019-07-08 08:39:16 -0400

    Update cal_xgb_proba.py

--- cal_xgb_proba.py
+++ cal_xgb_proba.py
@@ -1,4 +1,18 @@
 import numpy as np
 import random
-import xgboost as xgb
 from xgboost import XGBClassifier
+
+X=np.random.normal(1,1,[10,6])
+y=np.random.randint(2,size=10)
+
+model=XGBClassifier(learning_rate=0.1,n_estimators=2)
+model.fit(X,y)
+
+Xtest=np.random.normal(1,1,[2,6])
+ytest=np.random.randint(2,size=2)
+
+model.predict_proba(Xtest)
+model.get_booster().dump_model('output.txt')
+with open('output.txt','r') as f:
+    lmodel_leaves=f.read()
+print(model_leaves)
