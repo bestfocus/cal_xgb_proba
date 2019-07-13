84f3dc1701bfb484272c77174ce26301dbde121a
bestfocus <51957682+bestfocus@users.noreply.github.com>
2019-07-13 12:17:08 -0400

    Update cal_xgb_proba.py

--- cal_xgb_proba.py
+++ cal_xgb_proba.py
@@ -1,12 +1,15 @@
 import numpy as np
 from xgboost import XGBClassifier
 
+#simulate inputs for training the model
 X=np.random.normal(1,1,[10,6])
 y=np.random.randint(2,size=10)
 
+#use xgboost to train
 model=XGBClassifier(learning_rate=0.1,n_estimators=2)
 model.fit(X,y)
 
+#simulate test data
 Xtest=np.random.normal(1,1,[2,6])
 ytest=np.random.randint(2,size=2)
 #get prediction results
@@ -17,6 +20,6 @@ with open('output.txt','r') as f:
     lmodel_leaves=f.read()
 print(model_leaves)
 
-#check proba results
+#replicate proba results with tree leaf results
 #for each row, find the leaf value on each tree, there are two trees in this example
 #proba = 1/(1+exp(-(tree0_leaf+tree1_leaf))
